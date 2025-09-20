f05292de1db4a46804f4dce0222e98006d810be3
Felicia Sabatini <feliciasabatinigunawan2005@gmail.com>
2025-09-20 22:16:25 +0700

    Commit No.4 - Assignment

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -56,6 +56,90 @@ class MainActivity : AppCompatActivity() {
                     "Curious George",
                     "Award winning investigator",
                     "https://cdn2.thecatapi.com/images/bar.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Luna",
+                    "Loves climbing and chasing butterflies",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Simba",
+                    "King of the living room jungle",
+                    "https://cdn2.thecatapi.com/images/8k7.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.AmericanCurl,
+                    "Cleo",
+                    "Sweet but always curious about everything",
+                    "https://cdn2.thecatapi.com/images/6qi.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Milo",
+                    "Sleeps 20 hours a day, purrs the other 4",
+                    "https://cdn2.thecatapi.com/images/3me.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.BalineseJavanese,
+                    "Shadow",
+                    "Disappears the moment you look away",
+                    "https://cdn2.thecatapi.com/images/4oj.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.AmericanCurl,
+                    "Nala",
+                    "Gentle but loves to wrestle with her toys",
+                    "https://cdn2.thecatapi.com/images/5qg.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Oliver",
+                    "The foodie cat, always hunting snacks",
+                    "https://cdn2.thecatapi.com/images/b1t.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Daisy",
+                    "Playful and loves chasing yarn balls",
+                    "https://cdn2.thecatapi.com/images/c9a.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.AmericanCurl,
+                    "Leo",
+                    "Brave and adventurous, always exploring",
+                    "https://cdn2.thecatapi.com/images/bpc.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.ExoticShorthair,
+                    "Whiskers",
+                    "Mysterious wanderer with glowing eyes",
+                    "https://cdn2.thecatapi.com/images/6s0.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Bella",
+                    "Elegant and calm, queen of the house",
+                    "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.AmericanCurl,
+                    "Rocky",
+                    "Energetic and loves climbing furniture",
+                    "https://cdn2.thecatapi.com/images/34u.jpg"
                 )
             )
         )
